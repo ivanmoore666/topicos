01fd18d0a94f7dd4ff508821b6fc04ba00ef5b67
Gustavo Perez <g.ivan.pl@gmail.com>
2016-03-15 16:41:57 -0600

    commit 2

--- DemoEgit/src/com/aulajava/git/PruebaGit.java
+++ DemoEgit/src/com/aulajava/git/PruebaGit.java
@@ -6,6 +6,9 @@ public class PruebaGit {
 		
 			//Esto es una prueba para probar los repositorios y sacar un 10 :)
 			System.out.println("modific 1");
+			System.out.println("modific 2");
 			
+			
+			//Esto es una prueba 
 		}
 }
